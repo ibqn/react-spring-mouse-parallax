a389761033172ed18d3b04f53a385941f83d93e9
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-10-31 21:53:04 +0100

    make it work finally

---
+++ readme.md
@@ -0,0 +1,5 @@
+## App was created with 
+
+```shell
+yarn create react-app react-spring-mouse-parallax --typescript
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FunctionComponent, MouseEvent } from 'react';
 
-const App: React.FC = () => {
+import { useSpring, animated, interpolate } from 'react-spring';
+import './App.scss';
+
+
+const calc = (x: number, y: number) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
+const trans1 = (x: number, y: number): string => `translate3d(${x / 10}px,${y / 10}px,0)`;
+const trans2 = (x: number, y: number): string => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
+const trans3 = (x: number, y: number): string => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
+const trans4 = (x: number, y: number): string => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
+
+const App: FunctionComponent = () => {
+  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="container"
+      onMouseMove={({ clientX: x, clientY: y }: MouseEvent) => set({ xy: calc(x, y) })} >
+
+        <animated.div
+          className="card1"
+          style={{ transform: interpolate(props.xy, trans1) }}
+        />
+        <animated.div className="card2" style={{ transform: interpolate(props.xy, trans2) }} />
+        <animated.div className="card3" style={{ transform: interpolate(props.xy, trans3) }} />
+        <animated.div className="card4" style={{ transform: interpolate(props.xy, trans4) }} />
+
     </div>
   );
 }

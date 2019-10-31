a71d44d122c218730ea26405a7309cdfc930cfc6
Evgeny Bobkin <evgen.ibqn@gmail.com>
2019-10-31 22:07:54 +0100

    improve formatting. add screenshot

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 ```shell
 yarn create react-app react-spring-mouse-parallax --typescript
 ```
+
+![react-spring.png](react-spring.png)
--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,28 @@ import React, { FunctionComponent, MouseEvent } from 'react';
 import { useSpring, animated, interpolate } from 'react-spring';
 import './App.scss';
 
-
 const calc = (x: number, y: number) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const trans1 = (x: number, y: number): string => `translate3d(${x / 10}px,${y / 10}px,0)`;
-const trans2 = (x: number, y: number): string => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
-const trans3 = (x: number, y: number): string => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
-const trans4 = (x: number, y: number): string => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
+const trans1 = (x: number, y: number) => `translate3d(${x / 10}px,${y / 10}px,0)`;
+const trans2 = (x: number, y: number) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
+const trans3 = (x: number, y: number) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
+const trans4 = (x: number, y: number) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
 
 const App: FunctionComponent = () => {
-  const [props, set] = useSpring(() => ({ xy: [0, 0], config: { mass: 10, tension: 550, friction: 140 } }));
+  const [props, set] = useSpring(() => ({
+    xy: [0, 0],
+    config: { mass: 10, tension: 550, friction: 140 },
+  }));
+
   return (
     <div
       className="container"
-      onMouseMove={({ clientX: x, clientY: y }: MouseEvent) => set({ xy: calc(x, y) })} >
-
-        <animated.div
-          className="card1"
-          style={{ transform: interpolate(props.xy, trans1) }}
-        />
-        <animated.div className="card2" style={{ transform: interpolate(props.xy, trans2) }} />
-        <animated.div className="card3" style={{ transform: interpolate(props.xy, trans3) }} />
-        <animated.div className="card4" style={{ transform: interpolate(props.xy, trans4) }} />
-
+      onMouseMove={({ clientX: x, clientY: y }: MouseEvent) => set({ xy: calc(x, y) })}>
+      <animated.div className="card1" style={{ transform: interpolate(props.xy, trans1) }} />
+      <animated.div className="card2" style={{ transform: interpolate(props.xy, trans2) }} />
+      <animated.div className="card3" style={{ transform: interpolate(props.xy, trans3) }} />
+      <animated.div className="card4" style={{ transform: interpolate(props.xy, trans4) }} />
     </div>
   );
-}
+};
 
 export default App;

b35ece485e6cde23c1cdfff7cde6a8220192220b
ibqn <evgen.ibqn@gmail.com>
2021-10-23 10:30:12 +0200

    remove semis

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+import ReactDOM from 'react-dom'
+import App from './app'
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  const div = document.createElement('div')
+  ReactDOM.render(<App />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,19 @@
-import { FunctionComponent, MouseEvent } from 'react';
+import { FunctionComponent, MouseEvent } from 'react'
 
-import { useSpring, animated, interpolate } from 'react-spring';
-import './app.scss';
+import { useSpring, animated, interpolate } from 'react-spring'
+import './app.scss'
 
-const calc = (x: number, y: number) => [x - window.innerWidth / 2, y - window.innerHeight / 2];
-const trans1 = (x: number, y: number) => `translate3d(${x / 10}px,${y / 10}px,0)`;
-const trans2 = (x: number, y: number) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`;
-const trans3 = (x: number, y: number) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`;
-const trans4 = (x: number, y: number) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`;
+const calc = (x: number, y: number) => [x - window.innerWidth / 2, y - window.innerHeight / 2]
+const trans1 = (x: number, y: number) => `translate3d(${x / 10}px,${y / 10}px,0)`
+const trans2 = (x: number, y: number) => `translate3d(${x / 8 + 35}px,${y / 8 - 230}px,0)`
+const trans3 = (x: number, y: number) => `translate3d(${x / 6 - 250}px,${y / 6 - 200}px,0)`
+const trans4 = (x: number, y: number) => `translate3d(${x / 3.5}px,${y / 3.5}px,0)`
 
 const App: FunctionComponent = () => {
   const [props, set] = useSpring(() => ({
     xy: [0, 0],
     config: { mass: 10, tension: 550, friction: 140 },
-  }));
+  }))
 
   return (
     <div
@@ -24,7 +24,7 @@ const App: FunctionComponent = () => {
       <animated.div className="card3" style={{ transform: interpolate(props.xy, trans3) }} />
       <animated.div className="card4" style={{ transform: interpolate(props.xy, trans4) }} />
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
